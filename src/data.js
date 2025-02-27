17c7ee5e850706a9def14923e7bc5b61939a357a
nghi123-1 <tranhuunghi3210@gmail.com>
2025-02-27 15:01:42 +0700

    Cập nhật data.js và xóa danh sách sản phẩm

--- src/data.js
+++ src/data.js
@@ -2,97 +2,97 @@ export const data = [
      {
     catagoryName:'Monchinh',
     courseName:'Vịt Quay Bắc Kinh',
-    metaDiscription 'Món ăn đặc trưng ngon miệng',
-    imageUrl: require(./image/vitquaybackinh.jpg)
+    metaDiscription: 'Món ăn đặc trưng ngon miệng',
+    imageUrl: require('./image/vitquaybackinh.jpg')
 
 },
 {
     catagoryName:'Monchinh',
     courseName:'Gà Cung Bảo',
-    metaDiscription 'Món ăn đặc trưng ngon miệng',
-    imageUrl: require(./image/gacungbao.jpg)
+    metaDiscription: 'Món ăn đặc trưng ngon miệng',
+    imageUrl: require('./image/gacungbao.jpg')
 
 },
 {
     catagoryName:'Monchinh',
     courseName:'Thịt Kho Đông Pha',
-    metaDiscription 'Món ăn đặc trưng ngon miệng',
-    imageUrl: require(./image/thitkhodongpha.jpg)
+    metaDiscription: 'Món ăn đặc trưng ngon miệng',
+    imageUrl: require('./image/thitkhodongpha.jpg')
 
 },
 {
     catagoryName:'Monchinh',
     courseName:'Lẩu Tứ Xuyên',
-    metaDiscription 'Món ăn đặc trưng ngon miệng',
-    imageUrl: require(./image/laucaytuxuyen.jpg)
+    metaDiscription: 'Món ăn đặc trưng ngon miệng',
+    imageUrl: require('./image/laucaytuxuyen.jpg')
 
 },
 {
     catagoryName:'Monchinh',
     courseName:'Sủi Cảo',
-    metaDiscription 'Món ăn đặc trưng ngon miệng',
-    imageUrl: require(./image/suicao.jpg)
+    metaDiscription: 'Món ăn đặc trưng ngon miệng',
+    imageUrl: require('./image/suicao.jpg')
 
 },
 {
     catagoryName:'Monchinh',
     courseName:'Mì Trường Thọ',
-    metaDiscription 'Món ăn đặc trưng ngon miệng',
-    imageUrl: require(./image/mitruongtho.jpg)
+    metaDiscription: 'Món ăn đặc trưng ngon miệng',
+    imageUrl: require('./image/mitruongtho.jpg')
 
 },
 {
     catagoryName:'Monchinh',
     courseName:'Mì Hoành Thánh',
-    metaDiscription 'Món ăn đặc trưng ngon miệng',
-    imageUrl: require(./image/mihoanhthanh.jpg)
+    metaDiscription: 'Món ăn đặc trưng ngon miệng',
+    imageUrl: require('./image/mihoanhthanh.jpg')
 
 },
 
 {
     catagoryName:'Montrangmieng',
     courseName:'Kẹo Hồ Lô',
-    metaDiscription 'Món ăn đặc trưng ngon miệng',
-    imageUrl: require(./image/keoholo.jpg)
+    metaDiscription: 'Món ăn đặc trưng ngon miệng',
+    imageUrl: require('./image/keoholo.jpg')
 
 },
 
 {
     catagoryName:'Montrangmieng',
     courseName:'Màn Thầu Chiên',
-    metaDiscription 'Món ăn đặc trưng ngon miệng',
-    imageUrl: require(./image/manthauchien.jpg)
+    metaDiscription: 'Món ăn đặc trưng ngon miệng',
+    imageUrl: require('./image/manthauchien.jpg')
 
 },
 
 {
     catagoryName:'Montrangmieng',
     courseName:'Bánh Trôi Tàu',
-    metaDiscription 'Món ăn đặc trưng ngon miệng',
-    imageUrl: require(./image/banhtroitau.jpg)
+    metaDiscription: 'Món ăn đặc trưng ngon miệng',
+    imageUrl: require('./image/banhtroitau.jpg')
 
 },
 
 {
     catagoryName:'Montrangmieng',
     courseName:'Bánh Rán Vừng',
-    metaDiscription 'Món ăn đặc trưng ngon miệng',
-    imageUrl: require(./image/bảnhanvung.jpg)
+    metaDiscription: 'Món ăn đặc trưng ngon miệng',
+    imageUrl: require('./image/bảnhanvung.jpg')
 
 },
 {
     catagoryName:'Montrangmieng',
     courseName:'Bánh Chiên Xoắn',
-    metaDiscription 'Món ăn đặc trưng ngon miệng',
-    imageUrl: require(./image/banhchienxoan.jpg)
+    metaDiscription: 'Món ăn đặc trưng ngon miệng',
+    imageUrl: require('./image/banhchienxoan.jpg')
 
 },
 
 {
     catagoryName:'Montrangmieng',
     courseName:'Bánh Đường Vòng',
-    metaDiscription 'Món ăn đặc trưng ngon miệng',
-    imageUrl: require(./image/banhduongvong.jpg)
+    metaDiscription: 'Món ăn đặc trưng ngon miệng',
+    imageUrl: require('./image/banhduongvong.jpg')
 
 },
 
@@ -100,10 +100,10 @@ export const data = [
 {
     catagoryName:'Montrangmieng',
     courseName:'Kẹo Lạc',
-    metaDiscription 'Món ăn đặc trưng ngon miệng',
-    imageUrl: require(./image/keolac.jpg)
+    metaDiscription: 'Món ăn đặc trưng ngon miệng',
+    imageUrl: require('./image/keolac.jpg')
 
-},\\\
+},
 
 
 
